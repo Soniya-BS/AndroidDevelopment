a6ea1fe3efdafb60400870ecf9129fe2fea62e30
b31user06 <b31user06@rpsconsulting.in>
2024-05-15 12:03:13 +0530

    Projects added

---
+++ VehicleInsuranceApp/app/src/main/java/com/example/vehicleinsuranceapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.vehicleinsuranceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
